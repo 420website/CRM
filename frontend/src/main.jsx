3dae6ecd56efbe19e5f651075f46bc3b864c2da5
anthonyb8 <anthonybaxter819@gmail.com>
2025-09-04 13:19:09 -0400

    Added googleMaps loading function to utils and call in main.jsx so its only loaded once.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { loadGoogleMaps } from "./utils/utils";
+
+loadGoogleMaps();
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
