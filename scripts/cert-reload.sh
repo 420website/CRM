4964749de93ed81cbe878307b90aac3c0b458cd8
anthonyb8 <anthonybaxter819@gmail.com>
2025-10-06 10:16:18 -0400

    Testing cert-reload.sh as a service.

--- scripts/cert-reload.sh
+++ scripts/cert-reload.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-while inotifywait -e create /etc/vault/.cert-trigger 2>/dev/null; do
-  rm /etc/vault/.cert-trigger
+# Remove trigger if it exists
+rm -f /etc/vault/.cert-trigger
+
+while true; do
+  inotifywait -e create /etc/vault/.cert-trigger 2>/dev/null
+  rm -f /etc/vault/.cert-trigger
+  echo "$(date) - Detected trigger, restarting cert-dependent containers"
   docker compose --profile cert-reload up -d --force-recreate
 done
