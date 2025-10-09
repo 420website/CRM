cbca29ebd86df7e29a659fb826963561af41d4c5
anthonyb8 <anthonybaxter819@gmail.com>
2025-10-09 11:33:47 -0400

    Removed vconsole.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,19 @@ import "./App.css";
 import { HelmetProvider } from "react-helmet-async";
 import AppRoutes from "./routes";
 import { AuthProvider } from "./context/AuthContext";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 // import eruda from "eruda";
 
 function App() {
   // Only show on mobile or when debugging
-  useEffect(() => {
-    if (window.innerWidth < 768 || process.env.NODE_ENV === "development") {
-      import("vconsole").then((module) => {
-        const VConsole = module.default;
-        new VConsole();
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (window.innerWidth < 768 || process.env.NODE_ENV === "development") {
+  //     import("vconsole").then((module) => {
+  //       const VConsole = module.default;
+  //       new VConsole();
+  //     });
+  //   }
+  // }, []);
   // if (window.innerWidth < 768 || process.env.NODE_ENV === "development") {
   //   eruda.init();
   // }
