13c52ca6e8539730781ca19141915bf30dbe61f4
anthonyb8 <anthonybaxter819@gmail.com>
2025-10-09 11:30:06 -0400

    Swtich from eruda to vconsole.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,22 @@ import "./App.css";
 import { HelmetProvider } from "react-helmet-async";
 import AppRoutes from "./routes";
 import { AuthProvider } from "./context/AuthContext";
-import eruda from "eruda";
+import { useEffect } from "react";
+// import eruda from "eruda";
 
 function App() {
   // Only show on mobile or when debugging
-  if (window.innerWidth < 768 || process.env.NODE_ENV === "development") {
-    eruda.init();
-  }
+  useEffect(() => {
+    if (window.innerWidth < 768 || process.env.NODE_ENV === "development") {
+      import("vconsole").then((module) => {
+        const VConsole = module.default;
+        new VConsole();
+      });
+    }
+  }, []);
+  // if (window.innerWidth < 768 || process.env.NODE_ENV === "development") {
+  //   eruda.init();
+  // }
 
   return (
     <HelmetProvider>
