48b83b7adf2845a9a3d20666b0caea4d043a66dd
anthonyb8 <anthonybaxter819@gmail.com>
2025-11-01 13:18:29 -0400

    Added profiles to docker compose to always be crm.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@ set -e
 
 echo "Stoppping services..."
 # docker stop $(docker ps -aq)
-docker compose --profile "*" stop
+docker compose -p crm --profile "*" stop
 
 echo "Starting vault agent..."
-docker compose --profile vault-agent up -d --build
+docker compose -p crm --profile vault-agent up -d --build
 
 echo "Waiting for vault agent to complete current cycle..."
 timeout=60
@@ -43,7 +43,7 @@ source /etc/vault/secrets/.env
 set +a
 
 echo "Starting staging services..."
-docker compose --profile staging up -d --build
+docker compose -p crm --profile staging up -d --build
 
 echo "Waiting for certbot container to finish..."
 
@@ -85,10 +85,10 @@ else
 fi
 
 echo "Stopping staging services..."
-docker compose --profile staging stop
+docker compose -p crm --profile staging stop
 
 echo "Starting production services..."
-docker compose --profile prod up -d --build
+docker compose -p crm --profile prod up -d --build
 
 echo "Enabling cert and secert watch services..."
 sudo systemctl daemon-reload
