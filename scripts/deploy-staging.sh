97201b75f665a4542bb6470256038033cd93df8c
anthonyb8 <anthonybaxter819@gmail.com>
2026-07-10 22:10:33 -0400

    Added Caddy to replace Nginx

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -42,51 +42,6 @@ set -a
 source /etc/vault/secrets/.env
 set +a
 
-echo "Starting staging services..."
-docker compose -f compose.staging.yml --profile staging up -d --build
-
-echo "Waiting for certbot container to finish..."
-
-timeout=120
-interval=5
-elapsed=0
-
-while true; do
-  status=$(docker inspect --format='{{.State.Status}}' "$CERTBOT_CONTAINER" 2>/dev/null || echo "not_found")
-
-  if [ "$status" = "exited" ]; then
-    exit_code=$(docker inspect --format='{{.State.ExitCode}}' "$CERTBOT_CONTAINER")
-    if [ "$exit_code" -eq 0 ]; then
-      echo "Certbot container finished successfully."
-      break
-    else
-      echo "Certbot container exited with error code $exit_code."
-      exit 1
-    fi
-  fi
-
-  if [ "$elapsed" -ge "$timeout" ]; then
-    echo "Timeout waiting for certbot container to finish."
-    exit 1
-  fi
-
-  echo "Certbot container status: $status. Waiting... ($elapsed/$timeout seconds elapsed)"
-  sleep $interval
-  elapsed=$((elapsed + interval))
-done
-
-echo "Checking if certificate file exists..."
-
-if docker run --rm -v "$CERTBOT_VOLUME:/data" busybox sh -c "[ -f /data/live/$DOMAIN_ROOT/fullchain.pem ]"; then
-  echo "Certificate found!"
-else
-  echo "Certificate not found!"
-  exit 1
-fi
-
-echo "Stopping staging services..."
-docker compose -f compose.staging.yml --profile staging stop
-
 echo "Starting production services..."
 docker compose -f compose.staging.yml --profile prod up -d --build
 
